42baee3ff061a9d40a82018fce95e5410f222b64
Woosub Kim <woosubgo@gmail.com>
2024-05-01 14:48:42 +0900

    update CRISPR_bench

---
+++ CRISPR_benchmark/run_US-align_CRISPR_whole.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+
+open_sem() {
+    mkfifo pipe-$$
+    exec 3<>pipe-$$
+    rm pipe-$$
+    local i=$1
+    for ((;i>0;i--)); do
+        printf %s 000 >&3
+    done
+}
+
+# run the given command asynchronously and pop/push tokens
+run_with_lock() {
+    local x
+    # this read waits until there is something to read
+    read -u 3 -n 3 x && ((0==x)) || exit $x
+    (
+        ( "$@"; )
+        # push the return code of the command to the semaphore
+        printf '%.3d' $? >&3
+    )&
+}
+USALIGN=$1
+RESULT="./tmp"
+QUERY="./CRISPR_db/pHS1543_NZ_CP025815_Csf3_5Csf2_unrelaxed_rank_001_alphafold2_multimer_v3_model_5_seed_000.pdb"
+
+mkdir -p "${RESULT}/"
+
+task() {
+    ${USALIGN} "${QUERY}" "${FILE}" -mm 1 -ter 0 -mol prot -outfmt 2 >> ${RESULT}/result.txt
+}
+
+N=64
+open_sem $N
+pdb_list=$(mktemp)
+find ./CRISPR_db/PDBs -type f > $pdb_list
+
+while IFS= read -r FILE; do
+    run_with_lock task "$FILE";
+done < "$pdb_list"
+
+wait
+awk -v query=$QUERY '$1~/#PDBchain1/ {next;} $3>=0.5 {split(query, q, "/"); split($2, a, "/"); split(a[length(a)], b, ":"); print q[length(q)]"\t"b[1]"\t"$3}' tmp/result.txt > $2
+rm -rf "$RESULT"
+rm "$pdb_list"
---
+++ CRISPR_benchmark/run_foldseek-MM-TM_CRISPR.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir tmp
+/usr/bin/time $1 easy-complexsearch ./CRISPR_db/pHS1543_NZ_CP025815_Csf3_5Csf2_unrelaxed_rank_001_alphafold2_multimer_v3_model_5_seed_000.pdb ./CRISPR_db/pdb100 tmp/result tmp --threads 1  --alignment-type 1  --cluster-search 1 # -v 0 
+awk '$5>=0.5 {print $0}' tmp/result_report > $2
+rm -rf tmp
