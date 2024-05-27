441740ab07b31ebab71f6ebd3c31ce13e4539339
Woosub-Kim <59988419+Woosub-Kim@users.noreply.github.com>
2024-05-27 17:21:12 +0900

    Update run_3DComplexV7_runtime.sh

--- 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
@@ -12,7 +12,7 @@ while read line || [ -n "$line" ] ; do
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot 2>> tmp/us_time.log
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot -fast 2>> tmp/uf_time.log
     done < ../datasets/3DComplexV7_runtime_target.list 
-    /usr/bin/time $2 easy-complexsearch $query ./tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1 2>> fs_time.log
+    /usr/bin/time $2 easy-complexsearch $query ./tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1 2>> tmp/fs_time.log
     /usr/bin/time $2 easy-complexsearch $query ./tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1 --alignment-type 1 2>> tmp/ft_time.log
 done < ../datasets/3DComplexV7_runtime_query.list 
 awk '/CPU/ {split($3, a, "e"); split(a[1], b, ":"); s+=b[1]*60+b[2]}' END {print "US-align: "s} .tmp/us_time.log
