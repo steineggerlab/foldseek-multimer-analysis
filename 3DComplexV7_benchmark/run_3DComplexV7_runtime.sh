3401f8221a937c7e38f95bfb1fbc94aa00695534
Woosub-Kim <59988419+Woosub-Kim@users.noreply.github.com>
2024-05-27 15:08:15 +0900

    Update run_3DComplexV7_runtime.sh

--- 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
@@ -6,9 +6,9 @@ $2 createindex ./tmp/3DComplexV7_db ./tmp/tmp --threads $3
 $2 cluster ./tmp/3DComplexV7_db ./tmp/3DComplexV7_clu ./tmp/tmp --min-seq-id 0.9 -c 0.99 -e 0.00001 --threads $3
 $2 createclusearchdb ./tmp/3DComplexV7_db ./tmp/3DComplexV7_clu ./tmp/3DComplexV7_clusearchdb -threads $3
 while read line || [ -n "$line" ] ; do
-    query=../datasets/3DcomplexV7/PDBs/$line
+    query=../datasets/3DComplexV7/PDBs/$line
     while read line || [ -n "$line" ] ; do
-	target=../datasets/3DcomplexV7/PDBs/$line
+	target=../datasets/3DComplexV7/PDBs/$line
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot 2>> tmp/us_time.log
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot -fast 2>> tmp/uf_time.log
     done < ../datasets/3DComplexV7_runtime_target.list 
