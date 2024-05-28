2c7de85da445b47de7f23166d9330509e44db345
Woosub Kim <woosubgo@gmail.com>
2024-05-28 12:20:54 +0900

    parsing 3DcomplexV7 bench

--- 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
@@ -3,4 +3,5 @@ rm -rf ./tmp
 mkdir ./tmp
 /usr/bin/time $1 easy-complexsearch ./db/3DComplexV7_db ./db/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1
 # parsing > $2
+# awk '$5>=0.65 && $6>=0.65 {print $0}' ./tmp/3DComplexV7_aln_report > $2
 rm -rf ./tmp
