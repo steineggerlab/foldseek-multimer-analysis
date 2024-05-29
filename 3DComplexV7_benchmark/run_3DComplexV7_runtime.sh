b392c02c8fb8d8193b514d5fdc4c609fa2f52243
Woosub-Kim <59988419+Woosub-Kim@users.noreply.github.com>
2024-05-29 11:27:14 +0900

    Update run_3DComplexV7_runtime.sh [typo]

--- 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
@@ -9,8 +9,8 @@ while read line || [ -n "$line" ] ; do
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot 2>> $DIR/tmp/us_time.log
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot -fast 2>> $DIR/tmp/uf_time.log
     done < $DIR/../datasets/3DComplexV7_runtime_target.list 
-    /usr/bin/time $2 easy-complexsearch $query $DIR/tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1 2>> $DIR/tmp/fs_time.log
-    /usr/bin/time $2 easy-complexsearch $query $DIR/tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1 --alignment-type 1 2>> $DIR/tmp/ft_time.log
+    /usr/bin/time $2 easy-complexsearch $query $DIR/tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln $DIR/tmp/tmp --cluster-search 1 --threads 1 2>> $DIR/tmp/fs_time.log
+    /usr/bin/time $2 easy-complexsearch $query $DIR/tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln $DIR/tmp/tmp --cluster-search 1 --threads 1 --alignment-type 1 2>> $DIR/tmp/ft_time.log
 done < $DIR/../datasets/3DComplexV7_runtime_query.list 
 awk '/CPU/ {split($3, a, "e"); split(a[1], b, ":"); s+=b[1]*60+b[2]} END {print "US-align: "s}' $DIR/tmp/us_time.log
 awk '/CPU/ {split($3, a, "e"); split(a[1], b, ":"); s+=b[1]*60+b[2]} END {print "US-align (fast): "s}' $DIR/tmp/uf_time.log
