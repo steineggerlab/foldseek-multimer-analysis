7706b4bf0980e12be774fbd3c1c2450c8c1f9962
Woosub-Kim <59988419+Woosub-Kim@users.noreply.github.com>
2024-05-29 11:23:22 +0900

    Update run_3DComplexV7_runtime.sh [typo]

--- 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_runtime.sh
@@ -8,7 +8,7 @@ while read line || [ -n "$line" ] ; do
 	target=$DIR/../datasets/3DComplexV7/PDBs/$line
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot 2>> $DIR/tmp/us_time.log
 	/usr/bin/time $1 $query $target  -mm 1 -ter 0 -mol prot -fast 2>> $DIR/tmp/uf_time.log
-    done < $DIR../datasets/3DComplexV7_runtime_target.list 
+    done < $DIR/../datasets/3DComplexV7_runtime_target.list 
     /usr/bin/time $2 easy-complexsearch $query $DIR/tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1 2>> $DIR/tmp/fs_time.log
     /usr/bin/time $2 easy-complexsearch $query $DIR/tmp/3DComplexV7_clusearchdb ./tmp/3DComplexV7_aln ./tmp/tmp --cluster-search 1 --threads 1 --alignment-type 1 2>> $DIR/tmp/ft_time.log
 done < $DIR/../datasets/3DComplexV7_runtime_query.list 
