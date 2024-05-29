cffb1e78fc6407d2998650c8ab98c2fd75da4a85
Woosub Kim <woosubgo@gmail.com>
2024-05-29 16:15:19 +0900

    commit for pull

--- 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 rm -rf $DIR/tmp
 mkdir $DIR/tmp
-/usr/bin/time $1 easy-complexsearch $DIR/db/3DComplexV7_db $DIR/db/3DComplexV7_clusearchdb $DIR/tmp/3DComplexV7_aln $DIR/tmp/tmp --cluster-search 1 --threads 1
+/usr/bin/time $1 easy-complexsearch $DIR/db/3DComplexV7_db $DIR/db/3DComplexV7_clusearchdb $DIR/tmp/3DComplexV7_aln $DIR/tmp/tmp --cluster-search 1 --threads $2
 # parsing > $2
 # awk '$5>=0.65 && $6>=0.65 {print $0}' ./tmp/3DComplexV7_aln_report > $2
-rm -rf $DIR/tmp
+# rm -rf $DIR/tmp
