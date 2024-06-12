5ae89e98198eb26cbd69dccd2a9189d503a885f9
Woosub Kim <woosubgo@gmail.com>
2024-06-12 11:47:08 +0900

    add --db-load-mode

--- 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 rm -rf $DIR/tmp
 mkdir $DIR/tmp
-/usr/bin/time $1 easy-complexsearch $DIR/db/3DComplexV7_db $DIR/db/3DComplexV7_clusearchdb $DIR/tmp/3DComplexV7_aln $DIR/tmp/tmp --cluster-search 1 --threads $3
+/usr/bin/time $1 easy-complexsearch $DIR/db/3DComplexV7_db $DIR/db/3DComplexV7_clusearchdb $DIR/tmp/3DComplexV7_aln $DIR/tmp/tmp -v 0 --cluster-search 1 --db-load-mode 2 --threads $3 
 awk -F, 'FNR==1 {next;} {print $1; print $2}' $DIR/../datasets/3DComplexV7/homomer_pairs.csv | sort -u > $DIR/tmp/homomer.list
 awk '$5<0.65 && $6<0.65 {next;} $1<=$2 {print $1","$2} $1>$2{print $2","$1}' $DIR/tmp/3DComplexV7_aln_report | sort -u > $DIR/tmp/filtered_report
 awk -F,  'BEGIN {print "code1,code2"} FNR==NR {f[$0]=1; next;} ($1 in f)  && ($2 in f) {print $0}' $DIR/tmp/homomer.list $DIR/tmp/filtered_report > $2
--- CRISPR_benchmark/run_foldseek-MM-TM_CRISPR.sh
+++ CRISPR_benchmark/run_foldseek-MM-TM_CRISPR.sh
@@ -2,6 +2,6 @@
 DIR="$( cd "$( dirname "$0" )" && pwd -P )"
 rm -rf $DIR/tmp
 mkdir $DIR/tmp
-/usr/bin/time $1 easy-complexsearch $DIR/../datasets/CRISPR/pHS1543_NZ_CP025815_Csf3_5Csf2_unrelaxed_rank_001_alphafold2_multimer_v3_model_5_seed_000.pdb $DIR/db/pdb100_clusearchdb $DIR/tmp/result $DIR/tmp/tmp --threads 1  --cluster-search 1  -v 0 --alignment-type 1
+/usr/bin/time $1 easy-complexsearch $DIR/../datasets/CRISPR/pHS1543_NZ_CP025815_Csf3_5Csf2_unrelaxed_rank_001_alphafold2_multimer_v3_model_5_seed_000.pdb $DIR/db/pdb100_clusearchdb $DIR/tmp/result $DIR/tmp/tmp --threads 1  --cluster-search 1  -v 0 --alignment-type 1 --db-load-mode 2
 awk '$5>=0.5 {print $0}' $DIR/tmp/result_report > $2
 rm -rf $DIR/tmp
