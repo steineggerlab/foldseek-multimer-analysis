913cb2165d34b1414d99deada881163343736713
Woosub Kim <woosubgo@gmail.com>
2024-10-29 13:06:52 +0900

    update 3DComplexV7 benchmark

--- 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
+++ 3DComplexV7_benchmark/run_3DComplexV7_benchmark.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "$0" )" && pwd -P )"
-rm -rf $DIR/tmp
-mkdir $DIR/tmp
-/usr/bin/time $1 easy-complexsearch $DIR/db/3DComplexV7_db $DIR/db/3DComplexV7_clusearchdb $DIR/tmp/3DComplexV7_aln $DIR/tmp/tmp -v 0 --cluster-search 1 --db-load-mode 2 --threads $3
-awk -F, 'FNR==1 {next;} {print $1; print $2}' $DIR/../datasets/3DComplexV7/homomer_pairs.csv | sort -u > $DIR/tmp/homomer.list
-awk '$5<0.65 && $6<0.65 {next;} {split($1, a, "."); split($2, b, "."); h1=a[1]; h2=b[1]} h1<=h2 {print h1","h2} h1>h2{print h2","h1}' $DIR/tmp/3DComplexV7_aln_report | sort -u > $DIR/tmp/filtered_report
-awk -F,  'BEGIN {print "code1,code2"} FNR==NR {f[$0]=1; next;} ($1 in f)  && ($2 in f) {print $0}' $DIR/tmp/homomer.list $DIR/tmp/filtered_report > $2
- rm -rf $DIR/tmp
+DATA="$( cd "$( dirname "$0" )" && pwd -P )"/../datasets/3DComplexV7/
+FOLDSEEK=$1
+DB=$2
+RES=$3
+THR=$4
+DIR=$5
+mkdir $DIR
+/usr/bin/time $FOLDSEEK easy-complexsearch $DB/3DComplexV7_db $DB/3DComplexV7_clusearchdb $DIR/3DComplexV7_aln $DIR/tmp -v 3 --cluster-search 1 --db-load-mode 2 --monomer-include-mode 1 --threads $THR > $DIR/search.log  2> $DIR/runtime.txt 
+awk -F, 'FNR==1 {next;} {print $1; print $2}' $DATA/homomer_pairs.csv | sort -u > $DIR/homomer.list
+awk '$5<0.65 && $6<0.65 {next;} {split($1, a, "."); split($2, b, "."); h1=a[1]; h2=b[1]} h1<=h2 {print h1","h2} h1>h2{print h2","h1}' $DIR/3DComplexV7_aln_report | sort -u > $DIR/filtered_report
+awk -F,  'BEGIN {print "code1,code2"} FNR==NR {f[$0]=1; next;} ($1 in f)  && ($2 in f) {print $0}' $DIR/homomer.list $DIR/filtered_report > $RES
